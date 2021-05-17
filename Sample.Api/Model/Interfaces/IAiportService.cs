5878803c5364c3a7b20bd61688589198cd8ccc56
Vyacheslav Shengur <slavcho21@gmail.com>
2021-05-17 11:51:42 +0300

    Review and fix code

---
+++ Sample.Api/Model/Interfaces/IAiportService.cs
@@ -0,0 +1,27 @@
+﻿using Sample.Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sample.Api.App
+{
+    /// <summary>
+    /// Сервис получения информации по аэропортам
+    /// </summary>
+    public interface IAiportService: IDisposable
+    {
+        /// <summary>
+        /// Рассчитать дистанцию между аэропортами
+        /// </summary>
+        /// <param name="airports">Список аропортов</param>
+        /// <returns></returns>
+        IEnumerable<AirportPair> CalculateDistance(IEnumerable<Airport> airports);
+
+        /// <summary>
+        /// Получить список аэропортов
+        /// </summary>
+        /// <param name="cities">Список городов, по которым необходимо получить список аэропортов</param>
+        /// <returns></returns>
+        Task<IEnumerable<Airport>> GetAirportsAsync(string[] cities);
+    }
+}
---
+++ Sample.Api/Tests/Common/TestHelper.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Api.Tests.Common
+{
+    /// <summary>
+    /// Вспомогательный тип для тестов
+    /// </summary>
+    public static class TestHelper
+    {
+        /// <summary>
+        /// Получить корретно настроенный конфигурационный файл
+        /// </summary>
+        /// <returns></returns>
+        public static IConfiguration GetConfiguration()
+        {
+            var myConfiguration = new Dictionary<string, string>
+            {
+                {"Endpoints:AirportService", "https://homework.appulate.dev/api/" }
+            };
+
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(myConfiguration)
+                .Build();
+
+            return configuration;
+        }
+
+        /// <summary>
+        /// Получить некорретно настроенный конфигурационный файл
+        /// </summary>
+        /// <returns></returns>
+        public static IConfiguration GetIncorrectConfiguration()
+        {
+            var myConfiguration = new Dictionary<string, string>
+            {
+                {"Endpoints:AirportService", "https://example.org"}
+            };
+
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(myConfiguration)
+                .Build();
+
+            return configuration;
+        }
+    }
+}
